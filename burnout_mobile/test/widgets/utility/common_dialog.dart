b3cf829b04503eeed5ca444a3b52c9a887d6da30
yongggnd <yongkerkwith@gmail.com>
2022-07-03 16:45:12 +0700

    feat goldens test common dialog

--- burnout_mobile/test/widgets/utility/common_dialog.dart
+++ burnout_mobile/test/widgets/utility/common_dialog.dart
@@ -4,6 +4,7 @@ import 'package:burnout_mobile/styles/app_theme.dart';
 import 'package:burnout_mobile/widgets/utility/common_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
 
 void main() {
   group(CommonDialog, () {
@@ -77,44 +78,115 @@ void main() {
       await tester.tap(find.byKey(const Key('cancelButtonDialog')));
       expect(_completer.isCompleted, isTrue);
     });
-  });
 
-  testWidgets('Dialog should render correctly', (tester) async {
-    await tester.pumpWidget(
-      MaterialApp(
-        home: StatefulBuilder(
-            builder: (BuildContext context, StateSetter setState) {
-          return Scaffold(
-            body: GestureDetector(
-              onTap: () async {
-                await showDialog(
-                  context: context,
-                  builder: (_) => CommonDialog(
-                      titleText: 'titleText',
-                      contentText: 'contentText',
-                      submitButtonTile: 'submit',
-                      cancelButtonTile: 'cancel',
-                      submitButtonColor: AppTheme.greenPrimary100,
-                      cancelButtonColor: AppTheme.greyPrimary400,
-                      cancelButtonCallBack: () {},
-                      submitButtonCallBack: () {}),
-                );
-              },
-            ),
-          );
-        }),
-      ),
-    );
+    testWidgets('Cancel callback should work on complete', (tester) async {
+      final Completer _completer = Completer();
 
-    await tester.tap(find.byType(GestureDetector));
-    await tester.pump();
-    await tester.pump(const Duration(seconds: 1));
-    expect(find.byKey(const Key('titleDialog')), findsOneWidget);
-    expect(find.byKey(const Key('contentDialog')), findsOneWidget);
-    expect(find.byKey(const Key('cancelButtonDialog')), findsOneWidget);
-    expect(find.byKey(const Key('submitButtonDialog')), findsOneWidget);
+      await tester.pumpWidget(
+        MaterialApp(
+          home: StatefulBuilder(
+              builder: (BuildContext context, StateSetter setState) {
+            return Scaffold(
+              body: GestureDetector(
+                onTap: () async {
+                  await showDialog(
+                    context: context,
+                    builder: (_) => CommonDialog(
+                        titleText: 'titleText',
+                        contentText: 'contentText',
+                        submitButtonTile: 'submit',
+                        cancelButtonTile: 'cancel',
+                        submitButtonColor: AppTheme.greenPrimary100,
+                        cancelButtonColor: AppTheme.greyPrimary400,
+                        cancelButtonCallBack: () {},
+                        submitButtonCallBack: _completer.complete),
+                  );
+                },
+              ),
+            );
+          }),
+        ),
+      );
+      await tester.tap(find.byType(GestureDetector));
+      await tester.pump();
+      await tester.pump(const Duration(seconds: 1));
+      await tester.tap(find.byKey(const Key('submitButtonDialog')));
+      expect(_completer.isCompleted, isTrue);
+    });
+
+    testWidgets('Dialog should render correctly', (tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: StatefulBuilder(
+              builder: (BuildContext context, StateSetter setState) {
+            return Scaffold(
+              body: GestureDetector(
+                onTap: () async {
+                  await showDialog(
+                    context: context,
+                    builder: (_) => CommonDialog(
+                        titleText: 'titleText',
+                        contentText: 'contentText',
+                        submitButtonTile: 'submit',
+                        cancelButtonTile: 'cancel',
+                        submitButtonColor: AppTheme.greenPrimary100,
+                        cancelButtonColor: AppTheme.greyPrimary400,
+                        cancelButtonCallBack: () {},
+                        submitButtonCallBack: () {}),
+                  );
+                },
+              ),
+            );
+          }),
+        ),
+      );
+
+      await tester.tap(find.byType(GestureDetector));
+      await tester.pump();
+      await tester.pump(const Duration(seconds: 1));
+      expect(find.byKey(const Key('titleDialog')), findsOneWidget);
+      expect(find.byKey(const Key('contentDialog')), findsOneWidget);
+      expect(find.byKey(const Key('cancelButtonDialog')), findsOneWidget);
+      expect(find.byKey(const Key('submitButtonDialog')), findsOneWidget);
+    });
+
+    testGoldens('Dialog should render correctly', (tester) async {
+      await loadAppFonts();
+      await tester.pumpWidget(
+        MaterialApp(
+          home: StatefulBuilder(
+              builder: (BuildContext context, StateSetter setState) {
+            return Scaffold(
+              body: GestureDetector(
+                onTap: () async {
+                  await showDialog(
+                    context: context,
+                    builder: (_) => CommonDialog(
+                        titleText: 'titleText',
+                        contentText: 'contentText',
+                        submitButtonTile: 'submit',
+                        cancelButtonTile: 'cancel',
+                        submitButtonColor: AppTheme.greenPrimary100,
+                        cancelButtonColor: AppTheme.greyPrimary400,
+                        cancelButtonCallBack: () {},
+                        submitButtonCallBack: () {}),
+                  );
+                },
+              ),
+            );
+          }),
+        ),
+      );
+
+      await tester.tap(find.byType(GestureDetector));
+      await tester.pump();
+      await tester.pump(const Duration(seconds: 1));
+      await multiScreenGolden(tester, 'common_dialog', devices: [
+        const Device(
+          name: 'Phone',
+          size: Size(450, 300),
+        )
+      ]);
+    });
   });
 }
-
-
-
