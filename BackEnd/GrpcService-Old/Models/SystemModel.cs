b4241196e27a08de2ccb69e87c2d8d901e037a55
arinoldmoon <arin@oldmoon-th.com>
2022-07-08 18:06:00 +0700

    Delete BackEnd/GrpcService-Old directory
    Delete Old Version

--- BackEnd/GrpcService-Old/Models/SystemModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GrpcService.Models
-{
-    public class FactorConfig
-    {
-        public string DATE_FORMAT_STRING { get { return "dd/MM/yyyy HH:mm"; } }
-
-        public int OperationLogID { get; set; }
-    }
-}
